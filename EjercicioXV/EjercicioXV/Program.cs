f130eaf22772c89af7a5f300791d6bca5d9f72e7
JoseLlampa <62297243+JoseLlampa@users.noreply.github.com>
2020-04-07 17:24:55 -0300

    Se valida que la contraseña. Se usa && en el while porque queremos que sean False && Falase para salir del bucle

---
+++ EjercicioXV/EjercicioXV/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjercicioXV
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int intentos = 1;
+            int maxIntentos = 3;
+            string contraseña = "";
+            string usuario = "";
+
+            Console.WriteLine("Ingrese nombre de usuario");
+            usuario = Console.ReadLine();
+            Console.WriteLine("Ingrese contraseña");
+
+            do
+            {
+                contraseña = Console.ReadLine();
+                if (contraseña != usuario)
+                {
+                    Console.WriteLine("Acceso denegado. La contraseña no es correcta.");
+                }
+                intentos++;
+            } while (intentos<=maxIntentos && contraseña!=usuario); //PORQUE ES && Y NO ||
+
+            if (contraseña == usuario)
+            {
+                Console.WriteLine("Bienvenido " + usuario);
+            }
+            else
+            {
+                Console.WriteLine("Clave bloqueada");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
